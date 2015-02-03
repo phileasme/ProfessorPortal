d5bcc004ee6c243f85594be26fa040401de2dc7a
MKaras <maximilian.karasinski@kcl.ac.uk>
2015-02-03 18:06:22 +0000

    moved member first files to src/test

--- src/test/HelloWorld2.java
+++ src/test/HelloWorld2.java
@@ -1,10 +1,10 @@
-package pracoursework2;
+package test;
 
-public class Phileasfirstfile {
+public class HelloWorld2 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hello");
+		System.out.println("Hello, World!");
 	}
 
 }
