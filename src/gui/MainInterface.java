90f15860dc89407fa7758235e50c1f64d976b2d2
russiee <nikita.vorontsov@kcl.ac.uk>
2015-02-08 02:24:17 +0000

    Added All Students in StudentRecords to Scroll List

---
+++ src/gui/MainInterface.java
@@ -0,0 +1,92 @@
+ package gui;
+
+import java.awt.BorderLayout;
+
+import javax.swing.DefaultListModel;
+import javax.swing.JFrame;
+import javax.swing.JList;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.ListSelectionModel;
+
+import records.Student;
+import records.StudentRecords;
+
+/**
+ * Class to create an interface containing Scrollable list of students
+ * Student Search function and Information Pop Up window of each student
+ * 
+ * @author Nikita Vorontsov
+ *
+ */
+public class MainInterface extends JFrame {
+	
+	Student test = new Student(10, "tutor@tutormail.com", "Test", "studentemail@mail.com");
+	private DefaultListModel studentListModel;
+	private JList studentList;
+	private StudentRecords sr = new StudentRecords();
+	
+	/**
+	 * Constructor for the Interface, setting a size, visibility, exit function and creating the widgets
+	 * 
+	 */
+	public MainInterface(){
+		super("PRA Coursework - MNP");
+		createStudentList();
+		createMenuBar();
+		setSize(500,400);
+		setVisible(true);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
+	
+	/**
+	 * Creates the scrollable list of students by name and ID Number from StudentRecords
+	 * 
+	 */
+	private void createStudentList(){
+		setLayout(new BorderLayout());
+		JPanel studentPanel = new JPanel();
+		studentListModel = new DefaultListModel();
+		studentListModel.addElement(test.getName()+ "(" + test.getNumber() + ")");
+		for(int i = 0; i<sr.numOfStudents()-1; i++){
+			studentListModel.addElement(sr.returnStudent(i).getName() + "(" + sr.returnStudent(i).getNumber() + ")");
+		}
+		studentList = new JList(studentListModel);
+		studentList.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
+		JScrollPane studentScroll = new JScrollPane(studentList);
+		studentPanel.add(studentScroll);
+		add(studentScroll, BorderLayout.WEST);
+		
+	}
+	
+	/**
+	 * Creates the MenuBar - Sets to Frame
+	 */
+	private void createMenuBar(){
+		JMenuBar menuBar = new JMenuBar();
+		JMenu menu = new JMenu();
+		menuBar.add(menu);
+		setJMenuBar(menuBar);
+	}
+		
+	public static void main (String[] args){
+		MainInterface mi = new MainInterface();
+		StudentRecords sr = new StudentRecords();
+		System.out.println(sr.returnStudents());
+	}
+}
+
+/**
+JButton buttonTest = new JButton("Test");
+window.add(buttonTest);
+
+class StudentPopUpListener implements ActionListener {
+	public void actionPerformed(ActionEvent e) {
+		JOptionPane.showMessageDialog(window, "Test Student Dialog");
+	}
+}
+
+buttonTest.addActionListener(new StudentPopUpListener());
+*/
