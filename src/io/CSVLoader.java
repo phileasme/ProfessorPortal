c95502baa4cae7c236edd4a35fd2f5fe83ebc9fd
MKaras <maximilian.karasinski@kcl.ac.uk>
2015-02-11 21:25:58 +0000

    More work on requirement 5

--- src/io/CSVLoader.java
+++ src/io/CSVLoader.java
@@ -4,8 +4,17 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.Scanner;
+import java.util.Arrays;
+import java.util.List;
+import java.util.ArrayList;
+//import java.util.Set;
+import java.util.Map;
+import java.util.HashMap;
+import java.util.regex.Pattern;
 
 import records.StudentRecords;
+import records.Result;
+import records.Assessment;
 
 /**
  * An object which serves two functions: 1) to load anonymous marking codes and
@@ -17,25 +26,11 @@ import records.StudentRecords;
  */
 public class CSVLoader {
 
-	private int moduleIndex;
-	private int assIndex;
-	private int candKeyIndex;
-	private int markIndex;
-	private int gradeIndex;
-	
 	private StudentRecords studentRecords;
 	
 	/**
-	 * Constructs a CSVLoader which will load exam results.
-	 * 
-	 * @param filePath the path to the CSV file to be loaded
-	 */
-	public CSVLoader(String filePath) {
-		readCSV(filePath);
-	}
-	
-	/**
-	 * Constructs a CSVLoader which will load anonymous marking codes.
+	 * Constructs a CSVLoader which will load either anonymous marking codes or
+	 * exam results, depending on the length of the header row.
 	 * 
 	 * @param sr the StudentRecords from the main GUI window
 	 * @param filePath the path to the CSV file to be loaded
@@ -51,11 +46,14 @@ public class CSVLoader {
 		try {
 			sc = new Scanner(new BufferedReader(new FileReader(path)));
 			
-			String[] categories = clean(sc.nextLine()).split(",");
+			List<String> categories = Arrays.asList(clean(sc.nextLine()).split(","));
 			
-			if (categories.length == 2) {
+			if (categories.size() == 2) {
 				// file contains anonymous marking codes
 				loadMarkingCodes(categories, sc);
+			} else {
+				// file contains exam/CW results
+				loadExamResults(categories, sc);
 			}
 			
 		} catch (IOException e) {
@@ -76,7 +74,7 @@ public class CSVLoader {
 	 * @param firstRow the first row of the loaded CSV file
 	 * @param sc a Scanner that has read the first line of the loaded CSV file
 	 */
-	private void loadMarkingCodes(String[] firstRow, Scanner sc) {
+	private void loadMarkingCodes(List<String> firstRow, Scanner sc) {
 		
 		// keep track of whether codes have corresponding students on record
 		int loaded = 0;
@@ -85,7 +83,7 @@ public class CSVLoader {
 		// begin by storing first row. Need try/catch in case
 		// it's an unrecognised student
 		try {
-			studentRecords.returnStudent(firstRow[0]).setMarkingCode(firstRow[1]);
+			studentRecords.returnStudent(firstRow.get(0)).setMarkingCode(firstRow.get(1));
 			++loaded;
 		} catch (NullPointerException e) {
 			++unknown;
@@ -101,16 +99,56 @@ public class CSVLoader {
 				++unknown;
 			}
 		}
-		
+				
 		// TODO replace this with popup message box
 		System.out.println("Anonymous marking codes imported. " + loaded +
 				" codes were for known students; " + unknown + " codes were for unknown students");
 	}
 
 	/**
-	 * Removes any form of double quotation marks from the supplied string.
-	 * It accounts for the quotation marks put in by many popular spreadsheet
-	 * applications and simple text editors.
+	 * poop
+	 * 
+	 * @param categories the header row of the CSV file
+	 * @param sc a Scanner that has read the first line of the loaded CSV file
+	 */
+	private void loadExamResults(List<String> categories, Scanner sc) {
+		// first find locations in row of info that we want
+		int moduleIndex = categories.indexOf("Module");
+		int assIndex = categories.indexOf("Ass");
+		int candKeyIndex = categories.indexOf("Cand Key");
+		int markIndex = categories.indexOf("Mark");
+		int gradeIndex = categories.indexOf("Grade");
+
+		List<Result> results = new ArrayList<Result>();
+		
+		// determine if we are dealing with exam results or coursework results
+		String[] row = clean(sc.nextLine()).split(",");
+		boolean exam = Pattern.matches("^[a-zA-Z]\\w+", row[candKeyIndex]);
+		
+		Map<String, Assessment> assMap = new HashMap<String, Assessment>();
+		
+		if (exam) {
+			// file contains exam results, with anonymous marking codes
+			Result r = new Result(row[moduleIndex], row[assIndex], row[candKeyIndex], row[markIndex], row[gradeIndex]);
+			
+			while (sc.hasNextLine()) {
+				row = clean(sc.nextLine()).split(",");
+				
+			}
+			
+		} else { // file contains coursework results, with modified ID number
+			
+			// remove course indicator from end of ID number
+			String num = row[candKeyIndex].replaceFirst("/\\w$", "");
+			Result r = new Result(row[moduleIndex], row[assIndex], num, row[markIndex], row[gradeIndex]);
+		}
+		
+	}
+	
+	/**
+	 * Removes any form of double quotation marks and number signs ("#") from
+	 * the supplied string. It accounts for the quotation marks put in by many
+	 * popular spreadsheet applications and text editors.
 	 * 
 	 * @param s the string to be cleaned
 	 * @return the cleaned string
@@ -131,6 +169,7 @@ public class CSVLoader {
 			s = s.replaceAll(quote, empty);
 		}
 
-		return s;
+		// remove # now to make life easier later
+		return s.replaceAll("#", "");
 	}
 }
