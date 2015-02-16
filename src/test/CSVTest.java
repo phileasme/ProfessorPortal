8b1b02ffab064d6cc321e90c8566e60b31663607
MKaras <maximilian.karasinski@kcl.ac.uk>
2015-02-16 18:05:51 +0000

    made compatible with new CSVLoader

--- src/test/CSVTest.java
+++ src/test/CSVTest.java
@@ -16,23 +16,36 @@ public class CSVTest {
 		String codes1 = null;
 		String codes2 = null;
 		String results1 = null;
+		String results2 = null;
 		
 		if (os.equals("linux")) {
 			codes1 = root + "/csv/anoncodes1.csv";
 			codes2 = root + "/csv/anoncodes2.csv";
 			
 			results1 = root + "/csv/201314/4CCS1CS1.csv";
+			results2 = root + "/csv/201314/4CCS1ELA.csv";
 		} else if (os.equals("windows 7")) {
 			codes1 = ""+root + "\\csv\\anoncodes1.csv";
 			codes2 = ""+root + "\\csv\\anoncodes2.csv";
 			
 			results1 = root + "\\csv\\201314\\4CCS1CS1.csv";
+			results2 = root + "\\csv\\201314\\4CCS1ELA.csv";
 		}
 		
 		StudentRecords sr = new StudentRecords();
 		CSVLoader csv = new CSVLoader(sr, codes1);
+		csv.readCSV(CSVLoader.MARKING_CODES);
+		
 		csv = new CSVLoader(sr, codes2);
+		csv.readCSV(CSVLoader.MARKING_CODES);
+		
 		csv = new CSVLoader(sr, results1);
+		csv.readCSV(CSVLoader.RESULTS);
+		
+		csv = new CSVLoader(sr, results2);
+		csv.readCSV(CSVLoader.RESULTS);
+		
+		System.out.println("\nAfter loading, studentRecords.assessments has " + sr.numOfAssessments() + " assessments");
 		
 	}
 }
