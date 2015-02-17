c8292c3d101ffb2e8775de411c9bafd40774a584
MKaras <maximilian.karasinski@kcl.ac.uk>
2015-02-17 18:29:40 +0000

    Tables are now added when results are loaded.
    Fixed a few things in CSVLoader (storing correct ID number on CW
    results; no error message when trying to load exam results when clicked
    on "load marking codes", and visa versa).

--- src/gui/ResultsTabManager.java
+++ src/gui/ResultsTabManager.java
@@ -1,27 +1,56 @@
 package gui;
 
+import java.util.List;
+import java.util.ArrayList;
+import java.util.ListIterator;
+import java.util.Observable;
+import java.util.Observer;
+
+import javax.swing.JLabel;
 import javax.swing.JTabbedPane;
 import javax.swing.JTable;
 
-import java.util.Observable;
-import java.util.Observer;
-import java.util.List;
-
-import records.StudentRecords;
 import records.Assessment;
+import records.StudentRecords;
+import records.Result;
 
 public class ResultsTabManager extends JTabbedPane implements Observer {
 
 	private MainInterface frame;
+	private JLabel initialLabel;
 	
 	public ResultsTabManager(MainInterface frame, StudentRecords studentRecords) {
 		this.frame = frame;
 		studentRecords.addObserver(this);
+		
 	}
 
 	@Override
 	public void update(Observable o, Object assessments) {
-		List<Assessment> assessmentList = (List<Assessment>) assessments;
-		System.out.println("Just recieved notification of " + assessmentList.size() + " new assessments");
+		List<Assessment> assessmentList = (ArrayList<Assessment>) assessments;
+		
+		String[] columns = {"Student Number", "Student Name", "Mark", "Grade"};
+		
+		for (Assessment a : assessmentList) {
+			String tabName = a.toString();
+			
+			if (indexOfTab(tabName) >= 0) continue;
+			
+			String[][] results = new String[a.size()][4];
+			int rowIndex = 0;
+			
+			for (ListIterator<Result> it = a.listIterator(); it.hasNext(); ++rowIndex) {
+				Result r = it.next();
+				String studentNumber = r.getCandKey();
+				
+				results[rowIndex][0] = studentNumber;
+				results[rowIndex][1] = "bob jones";
+				results[rowIndex][2] = r.mark;
+				results[rowIndex][3] = r.grade;
+			}
+			
+			JTable table = new JTable(results, columns);
+			addTab(tabName, table);
+		}
 	}
 }
