75604600a5b614e43da34044afc6e158d6dbfdf2
Phileas <phileas.hocquard@kcl.ac.uk>
2015-02-26 19:33:15 +0000

    Student’s exam results displayed on PopUpWindow. Task 5.e

--- src/gui/PopUpWindow.java
+++ src/gui/PopUpWindow.java
@@ -1,14 +1,16 @@
 package gui;
 
 import java.awt.BorderLayout;
+import java.awt.Dimension;
 import java.awt.Font;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
-import java.awt.Insets;
 
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JScrollBar;
+import javax.swing.JScrollPane;
 import javax.swing.border.EmptyBorder;
 
 import records.Student;
@@ -27,7 +29,7 @@ public class PopUpWindow extends JFrame {
 	private String studentEmail;
 	private String studentNumber;
 	private String studentTutor;
-
+	private JScrollPane scrollResults;
 	/**
 	 * Create the frame using a parameter Student
 	 * 
@@ -53,11 +55,26 @@ public class PopUpWindow extends JFrame {
 		panel.setLayout(new GridBagLayout());
 		GridBagConstraints c = new GridBagConstraints();
 		
-		//For phileas
-		JPanel newPanel = new JPanel();
-		contentPane.add(newPanel, BorderLayout.SOUTH);
-		JLabel testJ = new JLabel("Test");
-		newPanel.add(testJ);
+		
+		JPanel resultPanel = new JPanel();
+		scrollResults = new JScrollPane(resultPanel);
+		scrollResults.setPreferredSize(new Dimension(100,40));
+		contentPane.add(scrollResults, BorderLayout.SOUTH);
+		JLabel results = new JLabel();
+		String oldAllResults = ""+stu.getAllResults();
+		String newAllResults = ""+oldAllResults.substring(0,oldAllResults.length()-1)+" % ]";
+		newAllResults =newAllResults.replace(""+stu.getNumber(),"");
+		newAllResults =newAllResults.replace(",","% , ");
+		if(stu.getAllResults().isEmpty()){
+
+			results = new JLabel("<html><h1 style='font-size:10px'>No exam results loaded.</h1></html>");
+		}
+		else{
+			
+			results = new JLabel("Results : "+newAllResults);
+		}
+		resultPanel.add(results,TOP_ALIGNMENT);
+		
 		
 		JLabel studentNameLabel = new JLabel(studentName);
 		studentNameLabel.setFont(new Font("Tahoma", Font.PLAIN, 20));
