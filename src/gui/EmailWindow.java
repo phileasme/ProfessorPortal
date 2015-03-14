16bcd136298a505fdc5f0b85a7572685ff32802a
russiee <nikita.vorontsov@kcl.ac.uk>
2015-03-14 17:42:39 +0000

    Implemented all panels/buttons/areas. Will need to implement next windows etc.

--- src/gui/EmailWindow.java
+++ src/gui/EmailWindow.java
@@ -1,19 +1,24 @@
 package gui;
 
 import java.awt.BorderLayout;
+import java.awt.Font;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+import java.awt.Insets;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.Map;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
+import javax.swing.JTextArea;
 
 import records.Student;
 import records.StudentRecords;
+
 import com.jidesoft.swing.CheckBoxList;
 
 public class EmailWindow extends JFrame {
@@ -27,9 +32,16 @@ public class EmailWindow extends JFrame {
 	
 	private JButton selectAll;
 	private JButton selectNone;
+	private JButton next;
 	
 	private Student[] studentsToEmail;
 	private ArrayList<Student> studentArray;
+	
+	private JLabel headerLabel;
+	private JLabel footerLabel;
+	
+	private JTextArea headerArea;
+	private JTextArea footerArea;
 	
 	public EmailWindow(StudentRecords sr) {
 		super("Send Email");
@@ -37,6 +49,7 @@ public class EmailWindow extends JFrame {
 		setLayout(new BorderLayout());
 		createStudentPanel();
 		createTextPanel();
+		createNextPanel();
 		setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
 		setSize(540,300);
 		setResizable(false);
@@ -83,7 +96,52 @@ public class EmailWindow extends JFrame {
 	}
 	
 	public void createTextPanel() {
+		headerFooterPanel = new JPanel();
 		
+		GridBagLayout gridBagLayout = new GridBagLayout();
+		gridBagLayout.columnWidths = new int[]{354, 0};
+		gridBagLayout.rowHeights = new int[]{47, 79, 41, 73, 0};
+		gridBagLayout.columnWeights = new double[]{1.0, Double.MIN_VALUE};
+		gridBagLayout.rowWeights = new double[]{0.0, 0.0, 0.0, 0.0, Double.MIN_VALUE};
+		headerFooterPanel.setLayout(gridBagLayout);
+		
+		JLabel lblHeader = new JLabel("Header: ");
+		GridBagConstraints gbc_lblHeader = new GridBagConstraints();
+		gbc_lblHeader.insets = new Insets(0, 0, 5, 0);
+		gbc_lblHeader.gridx = 0;
+		gbc_lblHeader.gridy = 0;
+		headerFooterPanel.add(lblHeader, gbc_lblHeader);
+		
+		JTextArea textArea = new JTextArea();
+		GridBagConstraints gbc_textArea = new GridBagConstraints();
+		gbc_textArea.insets = new Insets(0, 0, 5, 0);
+		gbc_textArea.fill = GridBagConstraints.BOTH;
+		gbc_textArea.gridx = 0;
+		gbc_textArea.gridy = 1;
+		headerFooterPanel.add(textArea, gbc_textArea);
+		
+		JLabel lblFooter = new JLabel("Footer: ");
+		GridBagConstraints gbc_lblFooter = new GridBagConstraints();
+		gbc_lblFooter.insets = new Insets(0, 0, 5, 0);
+		gbc_lblFooter.gridx = 0;
+		gbc_lblFooter.gridy = 2;
+		headerFooterPanel.add(lblFooter, gbc_lblFooter);
+		
+		JTextArea textArea_1 = new JTextArea();
+		GridBagConstraints gbc_textArea_1 = new GridBagConstraints();
+		gbc_textArea_1.fill = GridBagConstraints.BOTH;
+		gbc_textArea_1.gridx = 0;
+		gbc_textArea_1.gridy = 3;
+		headerFooterPanel.add(textArea_1, gbc_textArea_1);
+
+		this.add(headerFooterPanel, BorderLayout.CENTER);
+	}
+	
+	public void createNextPanel() {
+		bottomButtonPanel = new JPanel();
+		next = new JButton("Next:");
+		bottomButtonPanel.add(next);
+		this.add(bottomButtonPanel, BorderLayout.SOUTH);
 	}
 	
 }
